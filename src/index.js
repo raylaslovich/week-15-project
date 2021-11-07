d74a080d5a227122eefea18350397119cc11f13e
raylaslovich <ray.laslovich@gmail.com>
2021-11-06 18:37:35 -0600

    added componenets, REST, modified index.css

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import './index.css';
-import App from './App';
+// import App from './App';
+import HousesList from './components/HousesList'
 import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HousesList />
   </React.StrictMode>,
   document.getElementById('root')
 );
